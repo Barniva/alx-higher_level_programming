480be1e6c37d928d2e28717d0b19f6e96afb970e
Barniva <barnabasva1@gmail.com>
2023-07-05 19:18:48 +0000

    C&O

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,5 +1,5 @@
+
 #!/usr/bin/python3
-# 4-rectangle.py
 """Defines a Rectangle class."""
 
 
@@ -59,4 +59,14 @@ class Rectangle:
             return ("")
 
         rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
