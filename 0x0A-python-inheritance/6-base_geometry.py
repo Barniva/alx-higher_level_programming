787bfbe993fb3c1b196908d2b32aa0038ae3fd66
Barniva <barnabasva1@gmail.com>
2023-07-10 22:48:05 +0000

    py _i

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -3,6 +3,10 @@
 
 class BaseGeometry:
     """instance of geometry class"""
+
     def __init__(self):
         """initialize class"""
         pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
