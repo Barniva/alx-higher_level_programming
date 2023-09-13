1dfb8b00275cdb27aeee9a8b36d983b2c732168d
Barniva <barnabasva1@gmail.com>
2023-09-13 20:33:43 +0300

    update

--- 2-arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-// Write a script that prints a message depending of the number of arguments passed
-if (process.argv.slice(2).length < 1) {
-  console.log('No argument');
-} else if (process.argv.slice(2).length === 1) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
-}
