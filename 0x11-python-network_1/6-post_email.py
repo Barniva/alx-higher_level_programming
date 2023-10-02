aaab54a32b379b2195edac3699f697a190545dd8
Barniva <barnabasva1@gmail.com>
2023-10-02 12:49:38 +0300

    post

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""This python module contains a script takes in a URL, sends a
-request to the URL and displays the value of the variable
-X-Request-Id in the response header"""
-from requests import get
-from sys import argv
-
+""" Write a Python script that fetches https://alx-intranet.hbtn.io/status """
+import requests
+import sys
 
 if __name__ == "__main__":
-    print(get(argv[1]).headers.get('X-Request-Id'))
+    pay = {'email': sys.argv[2]}
+    result = requests.post(sys.argv[1], data=pay)
+    file = result.text
+    print(file)
