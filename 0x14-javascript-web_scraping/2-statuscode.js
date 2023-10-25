41febc9911997376ad84b6d0951dc5011f9ec9e9
Barniva <barnabasva1@gmail.com>
2023-10-26 00:31:30 +0300

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-
 const request = require('request');
-const args = process.argv;
-
-request(args[2], function (error, response, body) {
-  if (error) { console.log(error); }
-  console.log('code:', response && response.statusCode);
+request.get(process.argv[2]).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
 });
